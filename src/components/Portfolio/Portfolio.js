3abb40e15daee2eebd6ab190df1f7fd2b3a47b8b
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-19 21:43:32 +0530

    feat: add and stylize Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,31 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import linkArrow from "../../images/link-arrow.svg";
 function Portfolio() {
     return (
-        <></>
+        <section className='portfolio'>
+            <h3 className="portfolio__title">Портфолио</h3>
+            <ul className="portfolio__links">
+                <li className="portfolio__link">
+                    <Link className="portfolio__link-item" to='https://github.com/VincitQuiPatitur/how-to-learn'>
+                        <p className="portfolio__link-text">Статичный сайт</p>
+                        <img src={linkArrow} alt="Стрелка" className="portfolio__link-arrow"/>
+                    </Link>
+                </li>
+                <li className="portfolio__link">
+                    <Link className="portfolio__link-item" to='https://vincitquipatitur.github.io/russian-travel/'>
+                        <p className="portfolio__link-text">Адаптивный сайт</p>
+                        <img src={linkArrow} alt="Стрелка" className="portfolio__link-arrow"/>
+                    </Link>
+                </li>
+                <li className="portfolio__link">
+                    <Link className="portfolio__link-item" to='https://mesto.kostrova.nomoredomains.work/'>
+                        <p className="portfolio__link-text">Одностраничное приложение</p>
+                        <img src={linkArrow} alt="Стрелка" className="portfolio__link-arrow"/>
+                    </Link>
+                </li>
+            </ul>
+        </section>
     );
 }
 
