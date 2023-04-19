61ade13a664f4c6a68c2fb45143f46fed85e5c5f
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-19 20:51:55 +0530

    feat: add and stylize AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,25 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import myPhoto from "../../images/avatar.png";
+
 function AboutMe() {
     return (
-        <></>
+        <section className='student'>
+            <h2 className="about__title">Студент</h2>
+            <div className="student__block">
+                <div className="student__text-container">
+                    <div className="student__info">
+                        <h3 className="student__info-name">Арина Кострова</h3>
+                        <p className="student__info-job">Фронтенд-разработчик, 23 года</p>
+                        <p className="student__info-biography">Я родилась и живу в Санкт-Петербурге, закончила
+                            информационные системы и технологии с отличием. Полюбила веб-разработку и стала учиться в
+                            Яндекс.Практикуме, чтобы подтянуть свои знания. Нахожусь в активном поиске работы.</p>
+                    </div>
+                    <Link to='https://github.com/VincitQuiPatitur' className='student__link'>GitHub</Link>
+                </div>
+                <img src={myPhoto} className='student__photo' alt="Фотография со мной"/>
+            </div>
+        </section>
     );
 }
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Promo from "../Promo/Promo";
-import NavBar from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
-import Header from "../Header/Header";
 
 function Main() {
     return (
@@ -13,7 +11,6 @@ function Main() {
             <Promo/>
             <AboutProject/>
             <Techs/>
-            <NavBar/>
             <AboutMe/>
             <Portfolio/>
         </main>
