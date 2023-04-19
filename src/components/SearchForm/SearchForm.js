852b3799206f0d4ecb3a1e0298c5cc43824d5d9c
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-20 00:29:11 +0530

    feat: add and stylize SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,8 +1,18 @@
 import React from "react";
 function SearchForm() {
     return (
-        <section className="search-form">
-
+        <section className="search">
+            <form action="" className="search__form">
+                <input type="text" className="search__form-input" placeholder='Фильм'/>
+                <button className="search__form-button" type="submit"></button>
+            </form>
+            <div className="search__sorts">
+                <label className="search__sorts-switch">
+                    <input className="search__sorts-input" type="checkbox" />
+                        <span className="search__sorts-slider"></span>
+                </label>
+                <p className="search__sorts-text">Короткометражки</p>
+            </div>
         </section>
     );
 }
