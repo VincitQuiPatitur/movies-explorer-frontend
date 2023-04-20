ec19b66695f497efb28126a97ee90d35b7a241d8
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-20 16:41:50 +0530

    feat: add and stylize Profile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,24 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 function Footer() {
+    const location = useLocation();
+
     return (
-        <footer className="footer">
-            <p className="footer__info">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-            <div className="footer__under-line">
-                <p className="footer__copyright">&copy; 2023</p>
-                <div className="footer__links">
-                    <Link className="footer__link" to='https://practicum.yandex.ru/'>Яндекс.Практикум</Link>
-                    <Link className="footer__link" to='https://github.com/'>GitHub</Link>
-                </div>
-            </div>
-        </footer>
+        <>
+            {location.pathname !== '/profile' &&
+                <footer className="footer">
+                    <p className="footer__info">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+                    <div className="footer__under-line">
+                        <p className="footer__copyright">&copy; 2023</p>
+                        <div className="footer__links">
+                            <Link className="footer__link" to='https://practicum.yandex.ru/'>Яндекс.Практикум</Link>
+                            <Link className="footer__link" to='https://github.com/'>GitHub</Link>
+                        </div>
+                    </div>
+                </footer>
+            }
+        </>
     )
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,7 +2,25 @@ import React from "react";
 
 function Profile() {
     return (
-        <></>
+        <form className="profile">
+            {/*Подтянуть имя пользователя*/}
+            <h1 className="profile__greeting">Привет, пользователь!</h1>
+            <fieldset className="profile__form">
+                <label className="profile__form-label">
+                    <span className="profile__form-text">Имя</span>
+                    <input placeholder="Имя" type="text" className="profile__form-input"/>
+                    <span className="profile__form-error"></span>
+                </label>
+                <label className="profile__form-label">
+                    <span className="profile__form-text">E-mail</span>
+                    <input placeholder="E-mail" type="email" className="profile__form-input"/>
+                    {/*Пример вывода ошибки, убрать позже*/}
+                    <span className="profile__form-error">Текст должен быть не короче 2 симв. Длина текста сейчас: 1 символ</span>
+                </label>
+            </fieldset>
+            <button className="profile__update-button">Редактировать</button>
+            <a href="/signin" className="profile__signout-button">Выйти из аккаунта</a>
+        </form>
     );
 }
 
