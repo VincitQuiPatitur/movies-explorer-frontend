887de413241c510ef0edbf6b54afc5d5b15b6d1c
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-20 23:30:59 +0530

    feat: add and stylize authorization login and register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,19 @@
 import React from "react";
+import AuthorizationForm from "../AuthorizationForm/AuthorizationForm";
+import Logo from "../Logo/Logo";
 
 function Login() {
     return (
-        <></>
+        <div className="login">
+            <Logo />
+            <AuthorizationForm
+                title={'Рады видеть!'}
+                buttonText={'Войти'}
+                text={'Ещё не зарегистрированы?'}
+                link={"/signup"}
+                linkText={"Регистрация"}
+            />
+        </div>
     );
 }
 
