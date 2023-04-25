03b8885b33d7053714659848326e48551e43f56b
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-25 20:05:37 +0530

    fix: fix some bugs and format the code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,16 +5,17 @@ import InfoTooltip from "../InfoTooltip/InfoTooltip";
 
 function Login(props) {
     return (
-        <><div className="login">
-            <Logo/>
-            <AuthorizationForm
-                title={'Рады видеть!'}
-                buttonText={'Войти'}
-                text={'Ещё не зарегистрированы?'}
-                link={"/signup"}
-                linkText={"Регистрация"}
-            />
-        </div>
+        <>
+            <div className="login">
+                <Logo/>
+                <AuthorizationForm
+                    title={'Рады видеть!'}
+                    buttonText={'Войти'}
+                    text={'Ещё не зарегистрированы?'}
+                    link={"/signup"}
+                    linkText={"Регистрация"}
+                />
+            </div>
             <InfoTooltip
                 isOpen={props.isOpen}
                 onClose={props.onClose}
