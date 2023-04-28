e7c8a5917776aeb428786edc85808728869efdbc
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-04-28 23:58:33 +0530

    fix: change header depending on authorization

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,37 @@ import NavTab from "../NavTab/NavTab";
 function Header(props) {
     const location = useLocation();
 
-    return (
-        <>
+    return (props.isLoggedIn ?
+        (<header className={`header${location.pathname === '/' ? ` header_promo` : ``}`}>
+                    <div className="header__container">
+                        <Logo/>
+                        <div onClick={props.onClick} id='burger_button'
+                             className={`header__menu-burger ${props.isActive ? `header__menu-burger_active` : ``}`}>
+                            <span className="header__menu-burger-span"></span>
+                        </div>
+                        <Navigation/>
+                        <ProfileButton/>
+                        <NavTab
+                            isActive={props.isActive}
+                            closeNavTab={props.onClose}
+                        />
+                    </div>
+            </header>) : location.pathname === '/' ? (
+            <header className={'header header_promo'}>
+                <div className="header__container">
+                    <Logo/>
+                    <div className="header__menu">
+                        <Link className="header__signup" to='/signup'>
+                            Регистрация
+                        </Link>
+                        <Link className="header__signin" to='/signin'>
+                            Войти
+                        </Link>
+                    </div>
+                </div>
+            </header>) : null);
+
+      {/*  (<>
             {(location.pathname === '/' || location.pathname === '/movies' || location.pathname === '/saved-movies' || location.pathname === '/profile') ?
                 <header className={`header${location.pathname === '/' ? ` header_promo` : ``}`}>
                     <div className="header__container">
@@ -24,7 +53,7 @@ function Header(props) {
                                 </Link>
                             </div>
                         }
-                        {/* Переделать на props.LoggedIn */}
+                        {/!* Переделать на props.LoggedIn *!/}
                         {(location.pathname === '/movies' || location.pathname === '/saved-movies' || location.pathname === '/profile') &&
                             <>
                                 <div onClick={props.onClick} id='burger_button'
@@ -35,7 +64,7 @@ function Header(props) {
                                 <ProfileButton/>
                             </>
                         }
-                        {/* На данный момент есть ошибка на версии для плашета: при переходе через NavBar на страницу промо, бургер-меню исчезает. Исправить при использовании защищёных роутов, чтобы в случае, если пользователь авторизирован, появлялось бургер-меню, иначе отображались кнопки регистрации и авторизации */}
+                        {/!* На данный момент есть ошибка на версии для плашета: при переходе через NavBar на страницу промо, бургер-меню исчезает. Исправить при использовании защищёных роутов, чтобы в случае, если пользователь авторизирован, появлялось бургер-меню, иначе отображались кнопки регистрации и авторизации *!/}
                         <NavTab
                             isActive={props.isActive}
                             closeNavTab={props.onClose}
@@ -44,8 +73,8 @@ function Header(props) {
                 </header>
                 : null
             }
-        </>
-    );
+        </>);*/}
+
 }
 
 export default Header;
